d6c6effdf814bd61f7169740b816bc994cc290fe
KHassen91 <hassen.knani.91@gmail.com>
2021-09-15 00:28:18 +0100

    Updates

---
+++ precache-manifest.6ad27499ed0a47c3ce4460e91bf5d0e1.js
@@ -0,0 +1,238 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4761788d5acc640c43f4a53db550557c",
+    "url": "/The-Shouters-Worldwide/index.html"
+  },
+  {
+    "revision": "a222f68c406b69b09165",
+    "url": "/The-Shouters-Worldwide/static/css/13.e457176d.chunk.css"
+  },
+  {
+    "revision": "b2b3899d97cb50299837",
+    "url": "/The-Shouters-Worldwide/static/css/5.9522b9fe.chunk.css"
+  },
+  {
+    "revision": "6494426dffc811cebad9",
+    "url": "/The-Shouters-Worldwide/static/css/main.6c38025e.chunk.css"
+  },
+  {
+    "revision": "404ea9296dc136daeb40",
+    "url": "/The-Shouters-Worldwide/static/js/0.be867b78.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/The-Shouters-Worldwide/static/js/0.be867b78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cae2a4e9e25939c04b36",
+    "url": "/The-Shouters-Worldwide/static/js/10.d3de392d.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/The-Shouters-Worldwide/static/js/10.d3de392d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8ac8ab240ef276b6a53",
+    "url": "/The-Shouters-Worldwide/static/js/11.a3efa1d9.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/The-Shouters-Worldwide/static/js/11.a3efa1d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ed1304574fa17304414",
+    "url": "/The-Shouters-Worldwide/static/js/12.50482cb3.chunk.js"
+  },
+  {
+    "revision": "a222f68c406b69b09165",
+    "url": "/The-Shouters-Worldwide/static/js/13.ede21658.chunk.js"
+  },
+  {
+    "revision": "65c0aacb97fb1c8465ce",
+    "url": "/The-Shouters-Worldwide/static/js/14.39748be9.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/The-Shouters-Worldwide/static/js/14.39748be9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30e0e45e0efb711151bb",
+    "url": "/The-Shouters-Worldwide/static/js/15.ac1a1352.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/The-Shouters-Worldwide/static/js/15.ac1a1352.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "849e92a0905db560c190",
+    "url": "/The-Shouters-Worldwide/static/js/16.d24b4005.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/The-Shouters-Worldwide/static/js/16.d24b4005.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "855833069155370260aa",
+    "url": "/The-Shouters-Worldwide/static/js/17.a8b3cc9e.chunk.js"
+  },
+  {
+    "revision": "3ba68e7835c7ef731a39",
+    "url": "/The-Shouters-Worldwide/static/js/18.33ae4501.chunk.js"
+  },
+  {
+    "revision": "02e34e9739a4142cc871",
+    "url": "/The-Shouters-Worldwide/static/js/19.547b84fc.chunk.js"
+  },
+  {
+    "revision": "67e5cc9ac96126186c8f",
+    "url": "/The-Shouters-Worldwide/static/js/3.82bbc017.chunk.js"
+  },
+  {
+    "revision": "000d6c36287224a7ff48f88e64636454",
+    "url": "/The-Shouters-Worldwide/static/js/3.82bbc017.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "830b82b85c1e16471329",
+    "url": "/The-Shouters-Worldwide/static/js/4.bbf034d8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/The-Shouters-Worldwide/static/js/4.bbf034d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2b3899d97cb50299837",
+    "url": "/The-Shouters-Worldwide/static/js/5.c6c1bd66.chunk.js"
+  },
+  {
+    "revision": "d2b85c3d6cd12d635845060f24eec4f9",
+    "url": "/The-Shouters-Worldwide/static/js/5.c6c1bd66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "449eb46a4c7e7110cb80",
+    "url": "/The-Shouters-Worldwide/static/js/6.6d4b77d4.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/The-Shouters-Worldwide/static/js/6.6d4b77d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fecf578429f1dc4725b",
+    "url": "/The-Shouters-Worldwide/static/js/7.fe92cca0.chunk.js"
+  },
+  {
+    "revision": "089346f5f735a6abc99b",
+    "url": "/The-Shouters-Worldwide/static/js/8.a80c6a8c.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/The-Shouters-Worldwide/static/js/8.a80c6a8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18195cfdbc84e21b3317",
+    "url": "/The-Shouters-Worldwide/static/js/9.7262c83e.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/The-Shouters-Worldwide/static/js/9.7262c83e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6494426dffc811cebad9",
+    "url": "/The-Shouters-Worldwide/static/js/main.28f8935f.chunk.js"
+  },
+  {
+    "revision": "a8d946bd1dc85daef67d",
+    "url": "/The-Shouters-Worldwide/static/js/runtime-main.4e4a1b08.js"
+  },
+  {
+    "revision": "ee9f44ae9c9c440b6ba1f3548f5d9508",
+    "url": "/The-Shouters-Worldwide/static/media/bg-1.ee9f44ae.jpg"
+  },
+  {
+    "revision": "956c4627cff9c3fbd745c62e59f1ba95",
+    "url": "/The-Shouters-Worldwide/static/media/bg-2.956c4627.jpg"
+  },
+  {
+    "revision": "4f1b1c36b7bfda2018f98140b64a5985",
+    "url": "/The-Shouters-Worldwide/static/media/bg-3.4f1b1c36.jpg"
+  },
+  {
+    "revision": "5c8879cfc0978bab0f74696b6a6d7648",
+    "url": "/The-Shouters-Worldwide/static/media/bg-5.5c8879cf.jpg"
+  },
+  {
+    "revision": "178acd97836f1299ad0a79c9ee028775",
+    "url": "/The-Shouters-Worldwide/static/media/img-1.178acd97.jpg"
+  },
+  {
+    "revision": "73134494ad3cb229319beb8f5c9f1d32",
+    "url": "/The-Shouters-Worldwide/static/media/img-1.73134494.jpg"
+  },
+  {
+    "revision": "8ff5e74b414a115dd9bc5a005cdbced0",
+    "url": "/The-Shouters-Worldwide/static/media/img-1.8ff5e74b.jpg"
+  },
+  {
+    "revision": "11d5fbd43dd3480f4b5fba50158b8cb4",
+    "url": "/The-Shouters-Worldwide/static/media/img-2.11d5fbd4.jpg"
+  },
+  {
+    "revision": "6883de38a8d87eab96cf90b4b2f542c2",
+    "url": "/The-Shouters-Worldwide/static/media/img-2.6883de38.jpg"
+  },
+  {
+    "revision": "9af344966f24e39525b24d1a72e675d9",
+    "url": "/The-Shouters-Worldwide/static/media/img-2.9af34496.jpg"
+  },
+  {
+    "revision": "464a97694eaef388a84d86ef04803ecd",
+    "url": "/The-Shouters-Worldwide/static/media/img-3.464a9769.jpg"
+  },
+  {
+    "revision": "9614c4c87408d74eb12a5d7bd0e29a2d",
+    "url": "/The-Shouters-Worldwide/static/media/img-3.9614c4c8.jpg"
+  },
+  {
+    "revision": "c267a8f184f05d547f0fdf39aada52a6",
+    "url": "/The-Shouters-Worldwide/static/media/img-3.c267a8f1.jpg"
+  },
+  {
+    "revision": "543e322b8cd55c5ca03f079b6ee6e87a",
+    "url": "/The-Shouters-Worldwide/static/media/img-4.543e322b.jpg"
+  },
+  {
+    "revision": "c22dc0201f360b390a0f3235c5e4ee54",
+    "url": "/The-Shouters-Worldwide/static/media/img-5.c22dc020.jpg"
+  },
+  {
+    "revision": "d24bd1ad81c93d06a5dcbce90da43eb2",
+    "url": "/The-Shouters-Worldwide/static/media/img-6.d24bd1ad.jpg"
+  },
+  {
+    "revision": "28dfb7fdc671df6fd2752bd97fa8519d",
+    "url": "/The-Shouters-Worldwide/static/media/logo.28dfb7fd.png"
+  },
+  {
+    "revision": "304ef12ea00461a2498b686d956a5ae8",
+    "url": "/The-Shouters-Worldwide/static/media/map.304ef12e.png"
+  },
+  {
+    "revision": "187554eb3597e6ac3102b388b4038662",
+    "url": "/The-Shouters-Worldwide/static/media/materialdesignicons-webfont.187554eb.eot"
+  },
+  {
+    "revision": "a5a63c099747d5e3167dd87d1bef90ce",
+    "url": "/The-Shouters-Worldwide/static/media/materialdesignicons-webfont.a5a63c09.woff"
+  },
+  {
+    "revision": "af695aec1806605828e23a5afcf06f55",
+    "url": "/The-Shouters-Worldwide/static/media/materialdesignicons-webfont.af695aec.ttf"
+  },
+  {
+    "revision": "b65a7ff4c913b0ae8b715a3629fa463f",
+    "url": "/The-Shouters-Worldwide/static/media/materialdesignicons-webfont.b65a7ff4.svg"
+  },
+  {
+    "revision": "d1bdfb3838e2f78edf1ede85f56eabc9",
+    "url": "/The-Shouters-Worldwide/static/media/materialdesignicons-webfont.d1bdfb38.woff2"
+  }
+]);
